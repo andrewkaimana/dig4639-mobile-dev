866ac31a2ec55fa8efc603fc93d02b69b8798bf1
Andrew Kaimana <an345133@ucf.edu>
2020-02-17 10:20:50 -0500

    ICE-7: Jest Tests AddList

--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,9 +1,29 @@
-import { Sum } from './sum.js'
+import { Sum, AddList } from './MyMath.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+describe('Testing AddList', () => {
+  it('Returns the sum of a list of numbers', () =>
+  {
+    let list = [1,2,3,4];
+    let result = AddList(list);
+    expect(result).toBe(10);
+  })
 })
 
+describe('Testing Sum', () => {
+  it('Adds a string and a number and get undefined', () => {
+    var result = Sum("Test", 5);
+    expect(result).toBeUndefined();
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3) // tobe compares values passed in to the previous result
+  })
+  test('Border cases', () => { 
+    expect(Sum(-10, 5)).toBe(-5)
+    // expect(Sum(.1, .2)).toBe(.3) // doesn't work
+    expect(Sum(100000000000000, 100000000000000)).toBe(200000000000000)
+  })
+});
 // ICE 
 
 // Examples for Null
